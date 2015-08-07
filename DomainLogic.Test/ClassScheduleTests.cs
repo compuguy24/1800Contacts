57b996c0c0f1e5786a59fe7a8baa1241b71a0304
compuguy24 <masonbennett@hotmail.com>
2015-08-07 06:29:43 -0600

    Test Passes - Can create ClassSchedule

--- DomainLogic.Test/ClassScheduleTests.cs
+++ DomainLogic.Test/ClassScheduleTests.cs
@@ -7,5 +7,23 @@ namespace DomainLogic.Test
     public class ClassScheduleTests
     {
         private ClassSchedule _testClassSchedule;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _testClassSchedule = new ClassSchedule();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _testClassSchedule = null;
+        }
+
+        [TestMethod]
+        public void ConstructorAcceptsListOfCourses()
+        {
+            Assert.IsNotNull(_testClassSchedule);
+        }
     }
 }
