77abb8cbeb572d8bb2f38b330417abe552ad0724
compuguy24 <masonbennett@hotmail.com>
2015-08-07 07:15:44 -0600

    ListOfCoursesPropertyCannotAcceptInvalidData() - passes

--- DomainLogic/ClassSchedule.cs
+++ DomainLogic/ClassSchedule.cs
@@ -1,12 +1,32 @@
-﻿namespace DomainLogic
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DomainLogic
 {
     public class ClassSchedule
     {
+        private string[] _listOfCourses;
+
         public ClassSchedule(string[] listOfCourses)
         {
             ListOfCourses = listOfCourses;
         }
 
-        public string[] ListOfCourses { get; set; }
+        public string[] ListOfCourses
+        {
+            get { return _listOfCourses; }
+            set
+            {
+                if (!IsValidListOfCourses(value)) throw new ArgumentException("Course list contained invalid entries");
+                _listOfCourses = value;
+            }
+        }
+
+        private bool IsValidListOfCourses(string[] courses)
+        {
+            var rgx = new Regex(": ");
+            return courses.All(course => rgx.Matches(course).Count == 1);
+        }
     }
 }
