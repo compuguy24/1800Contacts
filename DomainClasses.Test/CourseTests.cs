bad9acb7e3d1c78615596b92d7c437208a4696f7
compuguy24 <masonbennett@hotmail.com>
2015-08-08 20:32:57 -0600

    Added CourseNodeWithNoDependency() and CourseNodeWithDependency() tests - passing

--- DomainClasses.Test/CourseTests.cs
+++ DomainClasses.Test/CourseTests.cs
@@ -12,5 +12,30 @@ namespace DomainClasses.Test
         {
             _testCourse = new Course("Test", null);
         }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _testCourse = null;
+        }
+
+        [TestMethod]
+        public void CourseNodeWithNoDependency()
+        {
+            Assert.IsNotNull(_testCourse);
+            Assert.AreEqual("Test", _testCourse.Title);
+            Assert.IsNull(_testCourse.Dependency);
+        }
+
+        [TestMethod]
+        public void CourseNodeWithDependency()
+        {
+            _testCourse.Dependency = new Course("TestDependency", null);
+            Assert.IsNotNull(_testCourse);
+            Assert.AreEqual("Test", _testCourse.Title);
+            Assert.IsNotNull(_testCourse.Dependency);
+            Assert.AreEqual("TestDependency", _testCourse.Dependency.Title);
+            Assert.IsNull(_testCourse.Dependency.Dependency);
+        }
     }
 }
---
+++ DomainClasses/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainClasses
+{
+    public class Course
+    {
+        public string Title { get; private set; }
+        public Course Dependency { get; set; }
+
+        public Course(string title, Course dependency)
+        {
+            Title = title;
+            Dependency = dependency;
+        }
+    }
+}
