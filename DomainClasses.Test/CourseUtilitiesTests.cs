7c324d57871807f678feda8c495bff5675762ff2
compuguy24 <masonbennett@hotmail.com>
2015-08-08 20:38:46 -0600

    Added CourseUtilitiesTests with GetCourseObjectFromStringWithNoDependency() fails - compile

---
+++ DomainClasses.Test/CourseUtilitiesTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainClasses.Test
+{
+    [TestClass]
+    public class CourseUtilitiesTests
+    {
+        private string _testCourseWithNoDependency = "A: ";
+
+        [TestMethod]
+        public void GetCourseObjectFromStringWithNoDependency()
+        {
+            var course = CourseUtilities.CreateCourse(_testCourseWithNoDependency);
+        }
+    }
+}
