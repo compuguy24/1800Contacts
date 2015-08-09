5aba5a41e33b00bc2ffd2ecef900b8a86900e39e
compuguy24 <masonbennett@hotmail.com>
2015-08-08 22:47:45 -0600

    Added implementation, Added CreateComplexUnsortedList() test - passes, Improved CreateBasicUnsortedList() test - passes

--- DomainLogic.Test/SortCoursesTests.cs
+++ DomainLogic.Test/SortCoursesTests.cs
@@ -1,4 +1,5 @@
-﻿using DomainClasses;
+﻿using System.Collections.Generic;
+using DomainClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DomainLogic.Test
@@ -10,16 +11,33 @@ namespace DomainLogic.Test
         public void SortBasicList()
         {
             var testList = CreateBasicUnsortedList();
-            var sortedList = SortCourses.Sort(testList);
+            var sortedList = SortCourses.Sort(testList, x => x.Dependency);
             Assert.AreEqual("A, B, D, C, E, F", sortedList);
         }
 
-        private Course[] CreateBasicUnsortedList()
+        [TestMethod]
+        public void SortComplexList()
+        {
+            var testList = CreateComplexUnsortedList();
+            var sortedList = SortCourses.Sort(testList, x => x.Dependency);
+            Assert.AreEqual("A, B, G, D, C, E, F, H", sortedList);
+        }
+
+        private static IEnumerable<Course> CreateBasicUnsortedList()
         {
             var a = new Course("B", new Course("A", null));
             var b = new Course("E", new Course("C", new Course("D", null)));
             var c = new Course("F", new Course("A", null));
             return new[] { a, b, c };
+        }
+
+        private static IEnumerable<Course> CreateComplexUnsortedList()
+        {
+            var a = new Course("B", new Course("A", null));
+            var b = new Course("E", new Course("C", new Course("D", new Course("G", null))));
+            var c = new Course("F", new Course("A", null));
+            var d = new Course("H", new Course("D", null));
+            return new[] { a, b, c, d };
         }
     }
 }
--- DomainLogic/SortCourses.cs
+++ DomainLogic/SortCourses.cs
@@ -7,11 +7,29 @@ namespace DomainLogic
 {
     public class SortCourses
     {
-        public static string Sort(IEnumerable<Course> courseList)
+        public static string Sort(IEnumerable<Course> courseList, Func<Course, Course> getDependencies)
         {
             var result = new StringBuilder();
+            var visited = new Dictionary<string, bool>();
+
+            foreach (var course in courseList)
+            {
+                Visit(course, getDependencies, result, visited);
+            }
 
             return result.ToString();
+        }
+
+        private static void Visit(Course course, Func<Course, Course> getDependencies, StringBuilder sorted, Dictionary<string, bool> visited)
+        {
+            if (visited.ContainsKey(course.Title)) return;
+
+            visited[course.Title] = true;
+            var dependency = getDependencies(course);
+            if (dependency != null) Visit(dependency, getDependencies, sorted, visited);
+
+            if (sorted.Length == 0) sorted.Append(course.Title);
+            else sorted.Append(", " + course.Title);
         }
     }
 }
