e89984de244056b11826b218fd2f704c59597c04
compuguy24 <masonbennett@hotmail.com>
2015-08-08 22:07:32 -0600

    Added SortBasicList() test - failing, ok... going to have to re-study topological sort... uggh

---
+++ DomainLogic.Test/SortCoursesTests.cs
@@ -0,0 +1,25 @@
+﻿using DomainClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainLogic.Test
+{
+    [TestClass]
+    public class SortCoursesTests
+    {
+        [TestMethod]
+        public void SortBasicList()
+        {
+            var testList = CreateBasicUnsortedList();
+            var sortedList = SortCourses.Sort(testList);
+            Assert.AreEqual("A, B, D, C, E, F", sortedList);
+        }
+
+        private Course[] CreateBasicUnsortedList()
+        {
+            var a = new Course("B", new Course("A", null));
+            var b = new Course("E", new Course("C", new Course("D", null)));
+            var c = new Course("F", new Course("A", null));
+            return new[] { a, b, c };
+        }
+    }
+}
---
+++ DomainLogic/SortCourses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DomainClasses;
+
+namespace DomainLogic
+{
+    public class SortCourses
+    {
+        public static string Sort(IEnumerable<Course> courseList)
+        {
+            var result = new StringBuilder();
+
+            return result.ToString();
+        }
+    }
+}
