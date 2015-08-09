6ee5276f0adee4ccf2c0034749c4784231033909
compuguy24 <masonbennett@hotmail.com>
2015-08-08 20:42:41 -0600

    Implemented CourseUtilities.CreateCourse static method, Added GetCourseObjectFromStringWithDependency() and improved GetCourseObjectFromStringWithNoDependency() passing

--- DomainClasses.Test/CourseUtilitiesTests.cs
+++ DomainClasses.Test/CourseUtilitiesTests.cs
@@ -6,11 +6,24 @@ namespace DomainClasses.Test
     public class CourseUtilitiesTests
     {
         private string _testCourseWithNoDependency = "A: ";
+        private string _testCourseWithDependency = "B: A";
 
         [TestMethod]
         public void GetCourseObjectFromStringWithNoDependency()
         {
             var course = CourseUtilities.CreateCourse(_testCourseWithNoDependency);
+            Assert.AreEqual("A", course.Title);
+            Assert.IsNull(course.Dependency);
+        }
+
+        [TestMethod]
+        public void GetCourseObjectFromStringWithDependency()
+        {
+            var course = CourseUtilities.CreateCourse(_testCourseWithDependency);
+            Assert.AreEqual("B", course.Title);
+            Assert.IsNotNull(course.Dependency);
+            Assert.AreEqual("A", course.Dependency.Title);
+            Assert.IsNull(course.Dependency.Dependency);
         }
     }
 }
---
+++ DomainClasses/CourseUtilities.cs
@@ -0,0 +1,12 @@
+﻿namespace DomainClasses
+{
+    public static class CourseUtilities
+    {
+        public static Course CreateCourse(string course)
+        {
+            var title = course.Split(':')[0].Trim();
+            var dependency = course.Split(':')[1].Trim();
+            return string.IsNullOrEmpty(dependency) ? new Course(title, null) : new Course(title, new Course(dependency, null));
+        }
+    }
+}
