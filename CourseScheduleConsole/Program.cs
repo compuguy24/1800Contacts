3ff8422152ad8da8f266439ed0c393ad298087f7
compuguy24 <masonbennett@hotmail.com>
2015-08-08 22:56:26 -0600

    Added IgnoreBasicCyclicDependency() and IgnoreComplexCyclicDependency() tests - pass, also added a Console Application

---
+++ CourseScheduleConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using DomainLogic;
+
+namespace CourseScheduleConsole
+{
+    public class Program
+    {
+        private static readonly string[] TestListOfCourses = {
+            "Introduction to Paper Airplanes: ",
+            "Advanced Throwing Techniques: Introduction to Paper Airplanes",
+            "History of Cubicle Siege Engines: Rubber Band Catapults 101",
+            "Advanced Office Warfare: History of Cubicle Siege Engines",
+            "Rubber Band Catapults 101: ",
+            "Paper Jet Engines: Introduction to Paper Airplanes"
+        };
+
+        static void Main(string[] args)
+        {
+            var consoleClassSchedule = (!args.Any()) ? new ClassSchedule(TestListOfCourses) : new ClassSchedule(args);
+            // Console.WriteLine(consoleClassSchedule.GetClassSchedule());
+            Console.ReadLine();
+        }
+    }
+}
--- DomainLogic.Test/SortCoursesTests.cs
+++ DomainLogic.Test/SortCoursesTests.cs
@@ -23,6 +23,22 @@ namespace DomainLogic.Test
             Assert.AreEqual("A, B, G, D, C, E, F, H", sortedList);
         }
 
+        [TestMethod]
+        public void IgnoreBasicCyclicDependency()
+        {
+            var testList = CreateBasicCyclicList();
+            var sortedList = SortCourses.Sort(testList, x => x.Dependency);
+            Assert.AreEqual("A, B", sortedList);
+        }
+
+        [TestMethod]
+        public void IgnoreComplexCyclicDependency()
+        {
+            var testList = CreateComplexCyclicList();
+            var sortedList = SortCourses.Sort(testList, x => x.Dependency);
+            Assert.AreEqual("B, C, A", sortedList);
+        }
+
         private static IEnumerable<Course> CreateBasicUnsortedList()
         {
             var a = new Course("B", new Course("A", null));
@@ -38,6 +54,18 @@ namespace DomainLogic.Test
             var c = new Course("F", new Course("A", null));
             var d = new Course("H", new Course("D", null));
             return new[] { a, b, c, d };
+        }
+
+        private Course[] CreateBasicCyclicList()
+        {
+            var a = new Course("B", new Course("A", new Course("B", null)));
+            return new[] { a };
+        }
+
+        private Course[] CreateComplexCyclicList()
+        {
+            var a = new Course("A", new Course("C", new Course("B", new Course("A", null))));
+            return new[] { a };
         }
     }
 }
